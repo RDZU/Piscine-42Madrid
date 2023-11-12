f0f855a33eb720691f40b87de636f6b0af04d108
Randolph David Zamora <davidzamorau@gmail.com>
2023-11-12 21:55:33 +0100

    C00

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: razamora <razamora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 22:06:40 by razamora          #+#    #+#             */
+/*   Updated: 2023/11/12 15:38:52 by razamora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar((a / 10) + 48);
+			ft_putchar((a % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((b / 10) + 48);
+			ft_putchar((b % 10) + 48);
+			if (a != 98 || b != 99)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}
+}
---
+++ C00/main.c
@@ -0,0 +1,22 @@
+// #include "ex00/ft_putchar.c"
+// #include "ex01/ft_print_alphabet.c"
+// #include "ex02/ft_print_reverse_alphabet.c"
+// #include "ex03/ft_print_numbers.c"
+ //#include "ex04/ft_is_negative.c"
+ //#include "ex05/ft_print_comb.c"
+//#include "ex06/ft_print_comb2.c"
+//#include "ex07/ft_putnbr.c"
+///void ft_putchar(char c);
+
+int main (void)
+{
+//	 ft_putchar('Z');
+//	 ft_print_alphabet();
+//	 ft_print_reverse_alphabet();
+//	 ft_print_numbers();	
+//	 ft_is_negative(-1);
+//	ft_print_comb();
+//	ft_print_comb2();
+//	 ft_putnbr(-2147488);
+}
+  
